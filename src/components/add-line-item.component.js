44d8673ca59f6b4b822c3f7d072ad3eef84019f2
Spencer <haze4man@gmail.com>
2023-09-25 16:39:06 -0600

    adding in purchase or income options

--- src/components/add-line-item.component.js
+++ src/components/add-line-item.component.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { addItem, listData, getTotal } from "../Services/custom";
 
-export default function AddLineItem({getList, totalFunction }) {
+export default function AddLineItem({ getList, totalFunction }) {
+  let isPurchase;
   const addItemSubmit = async (e) => {
     e.preventDefault();
     const itemName = document.getElementById("item-name").value;
-    const itemAmount = document.getElementById("item-amount").value;
+    document.getElementById("item-name").value = "";
+    let itemAmount = document.getElementById("item-amount").value;
+    document.getElementById("item-amount").value = "";
+    if (isPurchase) itemAmount = "-" + itemAmount;
 
     await addItem(itemName, itemAmount);
     const data = await listData();
@@ -14,14 +18,43 @@ export default function AddLineItem({getList, totalFunction }) {
     const totalData = await getTotal();
     totalFunction(await totalData);
     getList(await data.data);
-  }
+  };
+
+  const transactionTypeSelect = (e) => {
+    if (e.target.innerText === "Purchase") {
+      isPurchase = true;
+      document.getElementById("purchase-selection").style.backgroundColor = "transparent";
+      document.getElementById("income-selection").style.backgroundColor = "#99aec3";
+    } else {
+      isPurchase = false;
+      document.getElementById("income-selection").style.backgroundColor = "transparent";
+      document.getElementById("purchase-selection").style.backgroundColor = "#99aec3";
+    }
+    document.getElementById("add-item__form").style.display = "block";
+  };
 
   return (
     <div className="add-item__container container__width">
-      <form onSubmit={addItemSubmit}>
+      <div className="input-type__container">
+        <button
+          id="purchase-selection"
+          onClick={(e) => transactionTypeSelect(e)}
+          className="secondary-button"
+        >
+          Purchase
+        </button>
+        <button
+          id="income-selection"
+          onClick={(e) => transactionTypeSelect(e)}
+          className="secondary-button"
+        >
+          Income
+        </button>
+      </div>
+      <form id="add-item__form" onSubmit={addItemSubmit}>
         <div className="form-input__container">
           <div>
-            <label htmlFor="name">Name of transaction</label>
+            <label htmlFor="name">Name</label>
             <input required id="item-name" type="text" name="name"></input>
           </div>
           <div>
@@ -29,7 +62,9 @@ export default function AddLineItem({getList, totalFunction }) {
             <input required id="item-amount" type="text" name="amount"></input>
           </div>
         </div>
-        <button class="primary-button" type="submit">Add item</button>
+        <button className="primary-button" type="submit">
+          Add item
+        </button>
       </form>
     </div>
   );
