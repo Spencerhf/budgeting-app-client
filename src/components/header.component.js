f8fea543c6ab741b3ad7e8aff3d17a3a8ee012ef
Spencer <haze4man@gmail.com>
2024-02-29 22:59:34 -0600

    Styling updates

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,14 +1,12 @@
 import React from "react";
-import logo from "../Assets/budget-icon.png";
 
 export default function Header() {
   return (
     <header>
       <div className="header__container">
         <a href="/">
-          <img alt="budgeting logo" src={logo} />
+          <h2>Track It</h2> 
         </a>
-        <h2>Track It</h2> 
       </div>
     </header>
   );
