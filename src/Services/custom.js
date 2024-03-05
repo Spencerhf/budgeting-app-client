0fc76790ab28bf76e9c2c645ae6d6901e852e364
Spencer <haze4man@gmail.com>
2024-03-04 22:23:30 -0600

    Figuring out authentication:

--- src/Services/custom.js
+++ src/Services/custom.js
@@ -4,12 +4,20 @@ const apiUrl = "https://almost-a-budgeting-app-6b804606f213.herokuapp.com";
 // const apiUrl = "http://localhost:3000";
 
 async function getTotal() {
-  const response = await axios.get(`${apiUrl}/monthly-total`);
+  const response = await axios.get(`${apiUrl}/monthly-total`, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
+  });
   return response.data;
 }
 
 async function getRecentActivity() {
-  const response = await axios.get(`${apiUrl}/activity-list`);
+  const response = await axios.get(`${apiUrl}/activity-list`, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
+  });
   return response;
 }
 
@@ -23,17 +31,29 @@ async function getAllActivity() {
 }
 
 async function getMoneyInActivity() {
-  const response = await axios.get(`${apiUrl}/money-in`);
+  const response = await axios.get(`${apiUrl}/money-in`, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
+  });
   return response;
 }
 
 async function getMoneyOutActivity() {
-  const response = await axios.get(`${apiUrl}/money-out`);
+  const response = await axios.get(`${apiUrl}/money-out`, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
+  });
   return response;
 }
 
 async function removeItem(id) {
-  const response = await axios.post(`${apiUrl}/remove-list/${id}`);
+  const response = await axios.post(`${apiUrl}/remove-list/${id}`, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
+  });
   return response;
 }
 
@@ -41,12 +61,20 @@ async function addItem(name, amount) {
   const response = await axios.post(`${apiUrl}/add-item`, {
     name: name,
     amount: amount,
+  }, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
   });
   return response;
 }
 
 async function updateLineItem(updateObj, id) {
-  const response = await axios.post(`${apiUrl}/item-update/${id}`, updateObj);
+  const response = await axios.post(`${apiUrl}/item-update/${id}`, updateObj, {
+    headers: {
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA5NjEyMjAzfQ.WvltXBU2Fu14q2ncGebkRID-VxXe68_YFFDnv7WGPU0"
+    }
+  });
   return response;
 }
 
